4b9f657cf3390788b03fd38e6f9b268efb2ec925
root <root@gauss34.rikt.ru>
2020-02-29 13:05:37 +0700

    homework-4 с комментариями в sql файлах

--- homework-4/4-1.sql
+++ homework-4/4-1.sql
@@ -34,6 +34,23 @@ SELECT id, name, birthday, TIMESTAMPDIFF(YEAR, birthday, CURDATE()) AS age
 FROM users
 WHERE 1
 ORDER BY id;
+/*
+Результат:
++----+--------+------------+------+
+| id | name   | birthday   | age  |
++----+--------+------------+------+
+|  1 | user1  | 1980-01-01 |   40 |
+|  2 | user2  | 1981-02-03 |   39 |
+|  3 | user3  | 1982-05-06 |   37 |
+|  4 | user4  | 1985-01-03 |   35 |
+|  5 | user5  | 1986-05-01 |   33 |
+|  6 | user6  | 1987-09-30 |   32 |
+|  7 | user7  | 1991-03-31 |   28 |
+|  8 | user8  | 1993-12-12 |   26 |
+|  9 | user9  | 1996-09-01 |   23 |
+| 10 | user10 | 2001-07-26 |   18 |
++----+--------+------------+------+
+*/
 
 -- Считаем средний возраст всех пользователей в таблице users
 SELECT AVG(age)
@@ -41,6 +58,14 @@ SELECT AVG(age)
     SELECT id, name, birthday, TIMESTAMPDIFF(YEAR, birthday, CURDATE()) AS age
     FROM users WHERE 1
   ) as AVG;
+/*
+Результат:
+----------+
+| AVG(age) |
++----------+
+|  31.1000 |
++----------+
+*/
 
 -- Считаем округлённый до целого значения средний возраст всех пользователей в таблице users
 SELECT FLOOR(AVG(age))
@@ -48,3 +73,11 @@ SELECT FLOOR(AVG(age))
     SELECT id, name, birthday, TIMESTAMPDIFF(YEAR, birthday, CURDATE()) AS age
     FROM users WHERE 1
   ) as AVG;
+/*
+Результат:
++-----------------+
+| FLOOR(AVG(age)) |
++-----------------+
+|              31 |
++-----------------+
+*/
